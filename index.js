35ccb261ec37c2620f7d24d229d0439b7bf17907
Hamza1506 <hamzapervez397@gmail.com>
2023-09-08 21:39:32 +0500

    Q5 AND 6

---
+++ index.js
@@ -0,0 +1,31 @@
+//Question 1
+//Addition
+document.write("<b>Addition"+"<br><br></b>")
+var num1 =+prompt("Enter Num 1")
+var num2 =+prompt("Enter Num 2")
+var sum=num1+num2
+document.write("Sum of " + +num1 + " and " + +num2 + " is " + sum +"<br><br>")
+
+//Question 2
+//Subtraction
+document.write("<b>Subtraction"+"<br></b>")
+var sub=num1-num2
+document.write("<br>"+"Subtraction of " + +num1 + " and " + +num2 + " is " + sub + "<br><br>")
+
+//Question 2
+//Multiplication
+document.write("<b>Multiplication"+"<br></b>")
+var mul=num1*num2
+document.write("<br>"+"Multiplication of " + +num1 + " and " + +num2 + " is " + mul+ "<br><br>")
+
+//Question 2
+//Division
+document.write("<b>Division"+"<br></b>")
+var divi=num1/num2
+document.write("<br>"+"Division of " + +num1 + " and " + +num2 + " is " + divi+ "<br><br>")
+
+//Question 2
+//Modulus
+document.write("<b>Modulus"+"<br></b>")
+var mod=num1%num2
+document.write("<br>"+"Modulus of " + +num1 + " and " + +num2 + " is " + mod+ "<br><br>")
